af87617b482136a5da576207870cda7e258c86bd
Rangga-PR <ranggaputraraharja@gmail.com>
2020-05-06 03:50:21 +0700

    add connection to db

--- src/server.js
+++ src/server.js
@@ -1,18 +1,28 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const appServer = express();
+(async () => {
+  const appServer = express();
+  appServer.use(bodyParser.urlencoded({ extended: false }));
+  appServer.use(bodyParser.json());
 
-appServer.use(bodyParser.json);
+  await mongoose.connect(process.env.MONGO_DB, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  });
 
-appServer.post("/signup", (req, res) => {
-  res.json(req.body);
-});
+  appServer.post("/signup", (req, res) => {
+    res.json(req.body);
+  });
 
-appServer.get("/login", (req, res) => {
-  res.json(req.body);
-});
+  appServer.post("/signin", (req, res) => {
+    console.log(req.body);
+    res.json(req.body);
+  });
 
-appServer.listen(3000, () => {
-  console.log("server is running on port 3000");
-});
+  appServer.listen(3000, () => {
+    console.log("server is running on port 3000");
+  });
+})();
