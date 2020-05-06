875c786a17cf5b99f734f08dac77ee573b34d166
Rangga-PR <ranggaputraraharja@gmail.com>
2020-05-06 15:02:42 +0700

    add signin and signup

--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const UserModel = require("./model/user");
+
+const salt = 10;
 
 (async () => {
   const appServer = express();
@@ -11,15 +15,46 @@ const mongoose = require("mongoose");
   await mongoose.connect(process.env.MONGO_DB, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
+    useCreateIndex: true,
   });
 
-  appServer.post("/signup", (req, res) => {
-    res.json(req.body);
+  appServer.post("/signup", async (req, res) => {
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    const newUser = await new UserModel({
+      username: req.body.username,
+      password: hashedPassword,
+    });
+
+    newUser
+      .save()
+      .then(result => {
+        res.status(200).json({ message: "success", result });
+      })
+      .catch(err => {
+        if (err.code === 11000)
+          res.status(400).json({ message: "username already taken" });
+        else {
+          res.status(500).json({ message: "failed to register" });
+        }
+      });
   });
 
-  appServer.post("/signin", (req, res) => {
-    console.log(req.body);
-    res.json(req.body);
+  appServer.post("/signin", async (req, res) => {
+    await UserModel.findOne({ username: req.body.username })
+      .then(result => {
+        if (!result) {
+          res.status(404).json({ message: "this user is not found" });
+        } else {
+          bcrypt.compare(req.body.password, result.password, (err, valid) => {
+            valid
+              ? res.status(200).json({ message: "success", result })
+              : res.status(400).json({ message: "incorrect password" });
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ message: "failed to login" });
+      });
   });
 
   appServer.listen(3000, () => {
